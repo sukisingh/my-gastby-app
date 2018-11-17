6db226efd9d305c9be5ada8de7abe632bd83ddff
suki <sukisingh02@gmail.com>
2018-11-18 00:12:29 +0530

    basic layout change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,14 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <div>
-    <h1>Power to create anything</h1>
-    <p>We are global branding agency helping compnaies to communicate through vision and imagination</p> 
-    <Link to="/page-2/">Watch the video</Link>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Power to create anything</h1>
+          <p>We are global branding agency helping companies to communicate through vision and imagination</p> 
+          <Link to="/page-2/">Watch the video</Link>
+        </div>
+      </div>
+    
     </div>
     </Layout>    
 )
